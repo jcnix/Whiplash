ac712551c860ec5781d257b04e11df3288b90b5b
Casey Jones <jonescaseyb@gmail.com>
2011-09-19 17:40:02 -0500

    First working version
    BEWARE: Infinite Loop.

--- src/org/cjones/facebook/Main.java
+++ src/org/cjones/facebook/Main.java
@@ -1,9 +1,17 @@
 package org.cjones.facebook;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Contacts.People;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Data;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,9 +23,9 @@ import com.facebook.android.FacebookError;
 import com.facebook.android.DialogError;
 import com.facebook.android.Util;
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
+//import java.io.BufferedOutputStream;
+//import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -25,6 +33,8 @@ import java.net.URLConnection;
 import org.json.JSONException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.cjones.facebook.SessionEvents.AuthListener;
+import org.cjones.facebook.SessionEvents.LogoutListener;
 
 public class Main extends Activity
 {
@@ -40,9 +50,14 @@ public class Main extends Activity
         setContentView(R.layout.main);
 
         asyncRunner = new AsyncFacebookRunner(fb);
-
-        fb.authorize(this, new String[] {"friends_photos" },
-                new DialogListener()
+        SessionStore.restore(fb, this);
+        SessionEvents.addAuthListener(new SampleAuthListener());
+        SessionEvents.addLogoutListener(new SampleLogoutListener());
+        // Use this if we want to let users choose a photo instead
+        // of the Profile Picture in the future
+        //fb.authorize(this, new String[] {"friends_photos" },
+        //        new DialogListener()
+        fb.authorize(this, new DialogListener()
         {
             @Override
             public void onComplete(Bundle values)
@@ -73,7 +88,6 @@ public class Main extends Activity
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
-
         fb.authorizeCallback(requestCode, resultCode, data);
     }
 
@@ -89,6 +103,20 @@ public class Main extends Activity
     {
         public void onComplete(final String response, final Object state)
         {
+            String[] projection = new String[] {
+                People._ID,
+                People.NAME
+            };
+            Uri contacts = People.CONTENT_URI;
+            Cursor cursor = managedQuery(
+                    contacts,
+                    projection,
+                    null,
+                    null,
+                    People.NAME + " ASC"
+                    );
+            Log.w("FacebookSync", "Cursor: " + cursor.getCount());
+
             try
             {
                 JSONObject json = Util.parseJson(response);
@@ -97,21 +125,36 @@ public class Main extends Activity
                 for(int i = 0; i < data.length(); i++)
                 {
                     JSONObject frnd = data.getJSONObject(i);
-                    String name = frnd.getString("name");
+                    String fname = frnd.getString("name");
                     String id = frnd.getString("id");
-                    names += name + "\n";
+                    names += fname + "\n";
 
                     try
                     {
                         URL url = new URL("https://graph.facebook.com/"+id+"/picture");
                         URLConnection conn = url.openConnection();
                         BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
-                        BufferedOutputStream out = new BufferedOutputStream(
-                                new FileOutputStream("/mnt/sdcard/fbsync/"+id+".jpg"));
+                        ByteArrayOutputStream out = new ByteArrayOutputStream();
+                        //BufferedOutputStream out = new BufferedOutputStream(
+                        //        new FileOutputStream("/mnt/sdcard/fbsync/"+id+".jpg"));
                         int c;
                         while((c = in.read()) != -1)
                         {
                             out.write(c);
+                        }
+                        cursor.moveToFirst();
+                        while(cursor.moveToNext())
+                        {
+                            String cname = cursor.getString(1);
+                            if(cname == null)
+                                continue;
+                            Log.w("FacebookSync", cname);
+                            if(cname.equals(fname))
+                            {
+                                int cid = cursor.getInt(0);
+                                Uri per = ContentUris.withAppendedId(People.CONTENT_URI, cid);
+                                setPhoto(per, out.toByteArray(), Main.this);
+                            }
                         }
                     }
                     catch(IOException ex)
@@ -120,13 +163,6 @@ public class Main extends Activity
                     }
                 }
                 final String fnames = names;
-                Main.this.runOnUiThread(new Runnable()
-                {
-                    public void run()
-                    {
-                        mText.setText(fnames);
-                    }
-                });
             }
             catch(JSONException ex)
             {
@@ -136,6 +172,7 @@ public class Main extends Activity
             {
                 Log.w("FacebookSync", ex.getMessage());
             }
+            cursor.close();
         }
     }
 
@@ -150,6 +187,53 @@ public class Main extends Activity
         else
         {
             return false;
+        }
+    }
+
+    public void setPhoto(Uri personUri, byte[] photo, Context context)
+    {
+        ContentValues values = new ContentValues(); 
+        int photoRow = -1; 
+        String where = ContactsContract.Data.RAW_CONTACT_ID + " == " + 
+            ContentUris.parseId(personUri) + " AND " + Data.MIMETYPE + "=='" + 
+            ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE + "'"; 
+            Cursor cursor = context.getContentResolver().query 
+        (ContactsContract.Data.CONTENT_URI, null, where, null, null); 
+        int idIdx = cursor.getColumnIndexOrThrow(ContactsContract.Data._ID); 
+        if(cursor.moveToFirst())
+        { 
+            photoRow = cursor.getInt(idIdx); 
+        } 
+        cursor.close(); 
+        values.put(ContactsContract.Data.RAW_CONTACT_ID, 
+            ContentUris.parseId(personUri)); 
+        values.put(ContactsContract.Data.IS_SUPER_PRIMARY, 1); 
+        values.put(ContactsContract.CommonDataKinds.Photo.PHOTO, photo); 
+        values.put(ContactsContract.Data.MIMETYPE, 
+            ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE); 
+        if(photoRow >= 0)
+        { 
+            context.getContentResolver().update 
+                (ContactsContract.Data.CONTENT_URI, values, ContactsContract.Data._ID 
+                + " = " + photoRow, null); 
+        } else { 
+            context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values); 
+        } 
+    }
+
+    public class SampleAuthListener implements AuthListener {
+        public void onAuthSucceed() {
+        }
+
+        public void onAuthFail(String error) {
+        }
+    }
+
+    public class SampleLogoutListener implements LogoutListener {
+        public void onLogoutBegin() {
+        }
+
+        public void onLogoutFinish() {
         }
     }
 }
