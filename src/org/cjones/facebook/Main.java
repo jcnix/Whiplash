30b5670410466226205c3a32a79c35013a683460
Casey Jones <jonescaseyb@gmail.com>
2011-09-20 18:18:25 -0500

    Remove some imports from Main

--- src/org/cjones/facebook/Main.java
+++ src/org/cjones/facebook/Main.java
@@ -1,17 +1,9 @@
 package org.cjones.facebook;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.Contacts.People;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.Data;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
