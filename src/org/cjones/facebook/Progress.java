4e302924bd35e81f7baafd2791c385e37ad02272
Casey Jones <jonescaseyb@gmail.com>
2011-11-04 00:17:19 -0500

    Update the GUI to allow users to select which pictures to update
    Functionality will come later.

--- src/org/cjones/facebook/Progress.java
+++ src/org/cjones/facebook/Progress.java
@@ -6,28 +6,33 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Progress extends ListActivity implements Observer
 {
     private FriendsAdapter arr;
-    //private ArrayAdapter<Friend> arr;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         arr = new FriendsAdapter(this);
-        //arr = new ArrayAdapter<Friend>(this, R.id.text);
         setListAdapter(arr);
+        setContentView(R.layout.progress);
 
         ProgressObservable observable = ProgressObservable.getInstance();
         observable.addObserver(this);
@@ -50,11 +55,15 @@ public class Progress extends ListActivity implements Observer
     {
         private Activity activity;
         private ArrayList<Friend> friends;
+        private LayoutInflater vi;
+        private HashMap<int, boolean> checked;
 
         public FriendsAdapter(Activity a)
         {
             activity = a;
             friends = new ArrayList<Friend>();
+            vi = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            checked = new HashMap<int, bool>();
         }
 
         public int getCount() {
@@ -78,20 +87,87 @@ public class Progress extends ListActivity implements Observer
         @Override
         public View getView(int position, View convertView, ViewGroup parent)
         {
+            final ViewHolder holder;
+
             View v = convertView;
             if (v == null) {
-                LayoutInflater vi = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.progress, null);
+                v = vi.inflate(R.layout.progress_list, null);
+                
+                holder = new ViewHolder();
+                holder.image = (ImageView) v.findViewById(R.id.picture);
+                holder.cb = (CheckBox) v.findViewById(R.id.check);
+                holder.text = (TextView) v.findViewById(R.id.name);
+                
+                holder.text.setOnClickListener(new OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        holder.cb.setChecked(true);
+                    }
+                });
+
+                v.setTag(holder);
+            }
+            else
+            {
+                holder = (ViewHolder) v.getTag();
             }
 
             Friend fri = friends.get(position);
-            TextView text = (TextView) v.findViewById(R.id.text);
-            ImageView image = (ImageView) v.findViewById(R.id.image);
-            text.setText(fri.getName());
-            image.setImageBitmap(fri.getPhoto());
+            holder.ref = position;
+            holder.image.setImageBitmap(fri.getPhoto());
+            holder.text.setText(fri.getName());
+            
+            if(checked.containsKey(""+holder.ref))
+            { 
+                if(checked.get(""+holder.ref).equals("true"))
+                {
+                    holder.cb.setChecked(true);
+                }
+                else
+                    holder.cb.setChecked(false);
+            }
+            else
+                holder.cb.setChecked(false);
 
+            holder.cb.setOnCheckedChangeListener(new OncheckchangeListner(holder, checked));
             return v;
         }
     }
+}
+
+class OncheckchangeListner implements OnCheckedChangeListener
+{
+    private ViewHolder holder = null;
+    private HashMap<String, String> checked;
+
+    public OncheckchangeListner(ViewHolder viHolder, HashMap<String, String> checked)
+    {
+        holder =  viHolder;
+        this.checked = checked;
+    }
+    
+    @Override 
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    {
+        if(holder.cb.equals(buttonView))
+        {       
+            if(!isChecked)
+            {
+                checked.put(""+holder.ref,"false");
+            }
+            else
+                checked.put(""+holder.ref,"true");
+        }
+    }
+}
+
+class ViewHolder
+{
+    public ImageView image;
+    public CheckBox cb;
+    public TextView text;
+    public int ref;
 }
 
